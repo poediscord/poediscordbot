519158420211ca3e8e56c1d3f4be76208fb5c1ed
FWidm <fabian.widmann@uni-ulm.de>
2018-03-09 13:31:31 +0100

    fixed a bug in the parser where wrong item ids would be used to access items.

--- bot/parser.py
+++ bot/parser.py
@@ -76,7 +76,9 @@ class Parser:
                 item_id = Parser.get_attrib_if_exists(entry, 'itemId')
                 item = None
                 if item_id:
-                    item = items[int(item_id) - 1]
+                    print(item_id)
+                    #go through all items by their id, if the id matches return the first match of the comprehension.
+                    item = [item for item in  items if item.id == item_id][0]
                 slots[entry.attrib['name']] = ItemSlot(entry.attrib['name'], item_id, item)
 
         return slots
