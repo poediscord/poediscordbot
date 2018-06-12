91ac23fe413bcfb56c16a4329ee06c0dca2bad49
Fabian Widmann <fabian.widmann@uni-ulm.de>
2018-06-12 17:10:02 +0200

    bug(build checker): fixed requirements to only pass optional params if none are available

--- src/bot/util/build_checker.py
+++ src/bot/util/build_checker.py
@@ -3,7 +3,7 @@ from src.bot.consts.thresholds import OutputThresholds
 from src.models import Build
 
 
-def is_support(build: Build, dps, avg):
-    return (dps < OutputThresholds.DPS_SUPPORT or avg < OutputThresholds.AVG_SUPPORT) \
+def is_support(build: Build, dps=0, avg=0):
+    return (dps < OutputThresholds.DPS_SUPPORT.value or avg < OutputThresholds.AVG_SUPPORT.value) \
            and (build.aura_count > OutputThresholds.AURA_SUPPORT.value \
                 or build.curse_count > OutputThresholds.CURSE_SUPPORT.value)
