8db0ce4931b76b3b7e20d806054d5166cf668fd3
FWidm <fabian.widmann@gmail.com>
2019-06-06 18:05:00 +0200

    feat(pob-party): upgrade tree to 3.7.0

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -7,7 +7,7 @@ def starts_with(prefix, string):
     return bool(re.match(prefix, string, re.I))
 
 
-def fetch_xyz_pob_token(payload, version="3.6.0"):
+def fetch_xyz_pob_token(payload, version="3.7.0"):
     url = f"https://pob.party/kv/put?ver=v{version}"
     response = requests.post(url, data=payload)
     if response.status_code == 200:
