4cb8211319385d847aa106e7232db388d3a50c03
Kelossus <enk0pm@gmail.com>
2019-06-06 18:13:56 +0200

    Added the 3.7 tree

--- src/util/tree/poe_tree_codec.py
+++ src/util/tree/poe_tree_codec.py
@@ -6,7 +6,7 @@ from .poe_tree import PoeTree
 
 
 class PoeTreeCodec:
-    POE_TREE_JSON = 'resources/tree_3_5.json'
+    POE_TREE_JSON = 'resources/tree_3_7.json'
 
     def __init__(self):
         tree_data = json.load(open(config.ROOT_DIR + '/' + PoeTreeCodec.POE_TREE_JSON))
