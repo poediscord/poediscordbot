d0715114e70a4492128bc72e59a0865baf329aac
FWidm <fabian.widmann@gmail.com>
2019-06-07 17:56:01 +0200

    feat(pobparty): use latest instead of specifying the version

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -7,8 +7,14 @@ def starts_with(prefix, string):
     return bool(re.match(prefix, string, re.I))
 
 
-def fetch_xyz_pob_token(payload, version="3.7.0"):
-    url = f"https://pob.party/kv/put?ver=v{version}"
+def fetch_xyz_pob_token(payload, version="latest"):
+    """
+    fetch the token used for pob party
+    :param payload: pob payload
+    :param version: either v3.7.0 or latest for the latest one
+    :return:
+    """
+    url = f"https://pob.party/kv/put?ver={version}"
     response = requests.post(url, data=payload)
     if response.status_code == 200:
         return response.json()['url']
