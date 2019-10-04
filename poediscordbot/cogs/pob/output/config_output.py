4ef55b7d2dd2055cd98856d8dfbf5b448d6f5d97
fwidm <3480240+FWidm@users.noreply.github.com>
2019-10-04 12:10:31 +0200

    fix(config): add auto-updating of pob config + warning for unknown vals
    If pob updates new config options can exist, those need to get a group
    to be displayed. Additionally an abbreviation can be set. An appropriate
    warning is thrown if a config entry could not be displayed.

--- poediscordbot/cogs/pob/output/config_output.py
+++ poediscordbot/cogs/pob/output/config_output.py
@@ -1,3 +1,12 @@
+from poediscordbot.util.logging import log
+
+
+def special_ignored_case(name, value):
+    if name == 'Exposure to' and value == '0':
+        return True
+    return False
+
+
 def prepare_config_line(name, value):
     """
     Create the entry for one specific configuration with it's name and value
@@ -6,9 +15,10 @@ def prepare_config_line(name, value):
     :return: string
     """
     conf_item = f'{name}'
-    if value and value.lower() != 'true':
-        conf_item += f': {value.capitalize()}'
-    return conf_item
+    if not special_ignored_case(name, value):
+        if value and value.lower() != 'true':
+            conf_item += f': {value.capitalize()}'
+        return conf_item
 
 
 def get_config_string(config):
@@ -22,10 +32,14 @@ def get_config_string(config):
         abbrev = entry.get('abbreviation')
         value = entry.get('value')
         category = entry.get('category')
-        string = prepare_config_line(abbrev if abbrev else key, value)
-        if category:
-            configs.setdefault(category.capitalize(), []).append(string)
-
+        config_line = prepare_config_line(abbrev if abbrev else key, value)
+        if category and config_line:
+            configs.setdefault(category.capitalize(), []).append(config_line)
+        else:
+            log.warn(f"Category='{category}' or config_line='{config_line}' not set or unknown, check 'pob_conf.json'"
+                     f"for entries with mixing categories and abbreviations. "
+                     f"This happens after pob introduces new values. If one of the values is None you need to "
+                     f"update the json file similarly to the existing entries.")
     out = ''
     for category in configs:
         if len(configs[category]) > 0:
