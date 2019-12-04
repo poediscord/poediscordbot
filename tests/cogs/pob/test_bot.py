a23e3a681d8051255adb01556f115f1295f3a909
fwidm <3480240+FWidm@users.noreply.github.com>
2019-12-04 19:13:02 +0100

    fix(embed): ensure that embeds with invalid bodies are not built

--- tests/cogs/pob/test_bot.py
+++ tests/cogs/pob/test_bot.py
@@ -28,6 +28,23 @@ class TestBot(unittest.TestCase):
         build_embed = PoBCog._parse_pob(demo_author, demo_profile_link)
         self.assertFalse(isinstance(build_embed, Embed))
 
+    def test_empty_embed_field(self):
+        """
+        Empty names or values should not be carried into the embed. The pastebin has no secondary defense value
+        :return:
+        """
+        demo_profile_link = 'https://pastebin.com/X8XNW4EU'
+        demo_author = None
+        build_embed = PoBCog._parse_pob(demo_author, demo_profile_link)
+        self.assertTrue(isinstance(build_embed, Embed))
+        fields = build_embed._fields
+        for field in fields:
+            self.assertIsNotNone(field['name'])
+            self.assertIsNotNone(field['value'])
+
+        self.assertEqual([], [field['name'] for field in fields if "Secondary Defense" in field['name']],
+                         "Build should not have secondary defense field in embed")
+
 
 if __name__ == '__main__':
     unittest.main()
