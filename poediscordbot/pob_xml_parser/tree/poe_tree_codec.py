e9db80a0dab3593f97c9ffc967564dba5d813616
fwidm <3480240+FWidm@users.noreply.github.com>
2024-03-29 07:10:18 +0100

    feat(3.24): update tree & pob configs

--- poediscordbot/pob_xml_parser/tree/poe_tree_codec.py
+++ poediscordbot/pob_xml_parser/tree/poe_tree_codec.py
@@ -4,11 +4,11 @@ import struct
 
 from instance import config
 
-from .poe_tree import PoeTree
+from pob_xml_parser.tree.poe_tree import PoeTree
 
 
 class PoeTreeCodec:
-    POE_TREE_JSON = 'resources/tree_3_21.min.json'
+    POE_TREE_JSON = 'resources/tree_3_24.min.json'
 
     def __init__(self):
         tree_data = json.load(open(config.ROOT_DIR + '/' + PoeTreeCodec.POE_TREE_JSON))
